b2221f41d97275d83994cbb1d2f209ce6b9983bb
hetaorens <1656798028@qq.com>
2023-05-17 19:45:36 +0800

    add P1160

---
+++ OI/luogu/P1160.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<pair<int,int>> a;
+    a.push_back({-1,-1});
+    a.push_back({0,-1});
+    int N;
+    cin>>N;
+    int fn,bn;
+    for(int i=2;i<=N;++i)
+    {
+        cin>>fn>>bn;
+
+    }
+
+    return 0;
+}
